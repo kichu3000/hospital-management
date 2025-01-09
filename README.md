76cc1d631b427d8f99f40460bae109f78cde8410
unknown <anandakrishnanvb3000@gmail.com>
2025-01-09 19:57:20 +0530

    database in settings

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 Version used-
 python - 3.13.0
 django - 5.1.2
+postgresql used.
--- hospital/hospital/settings.py
+++ hospital/hospital/settings.py
@@ -77,8 +77,11 @@ WSGI_APPLICATION = 'hospital.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'hospital',
+        'USER': 'postgres',
+        'PASSWORD': 'admin',
+        'HOST': 'localhost',
     }
 }
 
