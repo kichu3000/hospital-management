1dc7cad08b27653a45fa894d652e03e636c1aba0
unknown <anandakrishnanvb3000@gmail.com>
2025-01-11 18:36:12 +0530

    login and signup page

--- hospital/home/urls.py
+++ hospital/home/urls.py
@@ -2,4 +2,6 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('',views.home,name='home'),
+    path('signup/',views.signup_view,name='signup'),
+    path('login/',views.login_view,name='login'),
 ]
--- hospital/home/views.py
+++ hospital/home/views.py
@@ -3,3 +3,10 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request,'home.html')
+
+
+def signup_view(request):
+    return render(request,'signup.html')
+
+def login_view(request):
+    return render(request,'login.html')
