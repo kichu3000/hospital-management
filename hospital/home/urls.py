6dfb0482cd2659bba4a5b8f90b9173890d6f4b9e
unknown <anandakrishnanvb3000@gmail.com>
2025-01-15 22:17:27 +0530

    Added appoinment

--- hospital/home/urls.py
+++ hospital/home/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('',views.home,name='home'),
+    #path('',views.home,name='home'),
+    path('',views.book_appointment,name='book_appointment'),
     path('signup/',views.signup_view,name='signup'),
     path('login/',views.login_view,name='login'),
     path('dashboard/',views.dashboard,name='dashboard'),#not created yet
+    
 ]
